7eaa5b35d7ae5db3f09b27dbbd9488731af81a3f
emilymeng <eeng@seattletimes.com>
2019-08-15 14:43:50 -0700

    working with data

--- src/js/main.js
+++ src/js/main.js
@@ -29,6 +29,27 @@ function initializeButtons(modalId, buttonId) {
             modal.style.display = "none";
         }
     }
+
+    var span = modal.getElementsByClassName("closeBottom")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+
 };
 
 initializeButtons("job", "btnJob");
