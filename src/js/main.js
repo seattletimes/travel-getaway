16b35f57b0c88ecc478c411d55958e8e1b5dd84f
emilymeng <eeng@seattletimes.com>
2019-08-15 01:27:57 -0700

    all data added

--- src/js/main.js
+++ src/js/main.js
@@ -3,28 +3,40 @@
 
 require("component-responsive-frame/child");
 
-// Get the modal
-var modal = document.getElementById("myDIV");
+function initializeButtons(modalId, buttonId) {
+    // Get the modal
+    var modal = document.getElementById(modalId);
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+    // Get the button that opens the modal
+    var btn = document.getElementById(buttonId);
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    // Get the <span> element that closes the modal
+    var span = modal.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-} 
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+};
+
+initializeButtons("job", "btnJob");
+initializeButtons("city", "btnCity");
+initializeButtons("routine", "btnRoutine");
+initializeButtons("weather", "btnWeather");
+initializeButtons("stress", "btnStress");
+initializeButtons("travel", "btnTravel");
+initializeButtons("family", "btnFamily");
+initializeButtons("tech", "btnTech");
+initializeButtons("comfort", "btnComfort");
